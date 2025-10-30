5bd6c09ac75300774d8666a8885722eb26f08885
yeon <yeonjae@email.com>
2025-10-30 16:36:54 +0900

    fastAPI 시작

--- 01_start/main.py
+++ 01_start/main.py
@@ -9,3 +9,9 @@ def main(): # 이 함수가 실행된다.
     # 아래 내용을 클라이언트로 전송
     # JSON 형태에 가까운 dictionary를 사용
     return {"message": "Hello, FastAPI! MY name is Lee,yeon-jae"}
+# uvicorn : 서버 프로그램 이름
+# main:app - main.py에 있는 app 사용
+# --host = 0.0.0.0 - 모든 IP 허용(이게 없으면 localhost, 127.0.0.1 만 허용)
+# --port = 3000 - 이 서버의 포트를 3000번으로 지정하겠다.
+# --reload - 소스 변경시 자동으로 서버를 재시작하겠다.
+# uvicorn main:app --host=0.0.0.0 --port=3000 --reload
