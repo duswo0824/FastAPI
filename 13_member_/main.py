5251adf7086c7f3ee95d789796fe0c614a14fe5d
yeon <yeonjae@email.com>
2025-11-06 15:38:09 +0900

    13_member_ 수업시간 session 처리

--- 13_member_/main.py
+++ 13_member_/main.py
@@ -1,6 +1,8 @@
 from typing import Dict, Any
 
 from fastapi import FastAPI
+from starlette.middleware.sessions import SessionMiddleware
+from starlette.requests import Request
 from starlette.staticfiles import StaticFiles
 
 from database import get_db
@@ -10,6 +12,13 @@ from sqlalchemy import text
 app = FastAPI()
 logger = Logger().get_logger(__name__)
 logger.info('log 준비완료')
+
+# pip install itsdangerous
+# session : cookie 처럼 어느 페이지에서나 공유 가능한 저장소
+# 서버에 저장되며, 브라우저가 바뀌거나 일정 시간이 지나면 자동 삭제됨
+# session 이 얼마나 저장되는지 알고 싶으면 다른 서비스 로그인 후 언제 로그인이 풀리는지 확인해보자
+# session 사용시 middleware 에 등록 필요
+app.add_middleware(SessionMiddleware, secret_key="session_secret_key")
 
 # app.add_middleware(CORSMiddleware,)
 # html이 py와 같은 서버(경로 view)에 있기 때문에 CORS 정책에서 자유로워졌다.
@@ -61,8 +70,9 @@ def join(info:Dict[str,Any]):
         return {'row':row} # 몇개가 데이터 반환 1 : insert 성공
 
 # 로그인
+# session 은 request 객체에 있다.
 @app.post('/login')
-def login(info:Dict[str,Any]):
+def login(info:Dict[str,Any],req:Request):
     logger.info(f'info={info}')
     sql = text("select count(id) as cnt from member where id = :id and pw = :pw;")
     db = None
@@ -72,6 +82,10 @@ def login(info:Dict[str,Any]):
         result = db.execute(sql, info).mappings().fetchone()# id와 pw를 둘다 만족하는 count 확인
         logger.info(f'result={result}')
         count = result['cnt'] # 해당 내용을 count 변수에 대입
+        
+        if count > 0 :
+            req.session['loginId'] = info['id'] # 로그인 했다는 증표로 loginId 라는 이름으로 id 값 저장
+
     except Exception as e :
         logger.error(e)
     finally:
@@ -80,10 +94,15 @@ def login(info:Dict[str,Any]):
 
 # 회원리스트
 @app.get('/list')
-def list(): # 매개변수 아무것도 없음
+def list(req:Request):
     member_list = []
     db = None
     sql = text("select id,name,gender from member;")
+    login_id = req.session.get('loginId','') # get(가져올값의 키, 없을 경우 만환할 값)
+    logger.info(f'login_id={login_id}')
+    if login_id == '': # session에 loginId 값이 없으면...
+        return {'list':member_list,'loginId':login_id}
+
     try:
         db = get_db()
         # 쿼리 실행 후 가져온 내용을 member_list 에 담아서 전송
@@ -93,4 +112,4 @@ def list(): # 매개변수 아무것도 없음
         logger.error(e)
     finally:
         db.close()
-        return {'list':member_list}
+        return {'list':member_list,'loginId':login_id}
