f4ed834c5d5db4a844f82db492554ac2a47a91b4
yeon <yeonjae@email.com>
2025-11-10 16:45:54 +0900

    14_board_파일 올리기file_save

--- 14_board/board.py
+++ 14_board/board.py
@@ -1,3 +1,7 @@
+import os
+import shutil
+import uuid
+
 from fastapi import params
 
 from util.database import get_db
@@ -29,6 +33,22 @@ def list(session):
         db.close()
         return {'list':list,'login':login_id}
 
+
+def file_save(files):
+    file_list = []
+    for file in files:
+        ori_filename = file.filename
+        # img01.png -> 12345678.png (확장자는 유지)
+        ext = os.path.splitext(ori_filename)[1] # img01.png -> [img01,.png] 1번인덱스에 확장자
+        new_filename = f'{uuid.uuid4()}{ext}'
+
+        with open('upload/'+new_filename,'wb') as tmp:
+            shutil.copyfileobj(file.file,tmp)
+
+        file_list.append({'ori_filename':ori_filename,'new_filename':new_filename})
+        logger.info(f'file_list = {file_list}')
+    return file_list
+
 def write(subject, user_name, content, files):
     result = {'success':'false'}
     # bbs 에 subject, user_name, content 저장
@@ -43,6 +63,13 @@ def write(subject, user_name, content, files):
          logger.info(f'exec_result:{exec_result.lastrowid}')
          if exec_result.rowcount > 0:
              result['success'] = 'true'
+             # 파일들을 전달하면 저장하고 생성된 새로운 이름을 받는다.
+             file_list = file_save(files)
+             # 파일의 이름, 새로 생성된 이름, idx를  photo 에 저장
+             sql = text('insert into photo(ori_filename,new_filename,idx)values(:ori_filename,:new_filename,:idx)')
+             for file in file_list:
+                 file['idx'] = exec_result.lastrowid
+                 conn.execute(sql, file)
              conn.commit()
     except Exception as e:
         logger.error(e)
