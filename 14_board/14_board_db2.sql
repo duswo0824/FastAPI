990ded99f21879ba6d7f7475b021f69b129d2ba3
yeon <yeonjae@email.com>
2025-11-10 16:59:29 +0900

    14_board_리스트 상세보기

---
+++ 14_board/14_board_db2.sql
@@ -0,0 +1,27 @@
+select*from bbs;
+
+insert into bbs(subject,user_name,content)values(:subject,:user_name,:content);
+insert into photo(ori_filename,new_filename,idx)values(:ori_filename,:new_filename,:idx);
+
+create table photo (
+    file_idx int(8) primary key auto_increment,
+    ori_filename varchar(100),
+    new_filename varchar(100),
+    idx int(8),
+    foreign key(idx) references bbs(idx)
+);
+
+desc photo;
+
+-- bbs 에 다음과 같은 내용을 넣으려고 한다.
+-- subject : 파일올리기
+-- user_name : 테스터
+-- content : 글도 올리고 파일도 올리고
+insert into bbs(subject,user_name,content)values('파일올리기','테스터','글도 올리고 파일도 올리고');
+select*from bbs;
+
+-- 그리고 photo에 사진을 한장 올리려고 한다.
+-- ori_filename : test.png
+-- new_filename : 123456789.png
+insert into photo(ori_filename,new_filename,idx)values('test.png','123456789.png',3);
+select*from photo;
