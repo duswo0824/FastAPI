548460b9853c9bc4a296d5a67c39e3fdc2ac9a93
yeon <yeonjae@email.com>
2025-11-13 14:12:31 +0900

    17_Selenium 로그인

--- 17_Selenium/main.py
+++ 17_Selenium/main.py
@@ -1,12 +1,17 @@
+import time
+
 from fastapi import FastAPI
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chromium.options import ChromiumOptions
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as ec #예상조건
+from selenium.webdriver.support.wait import WebDriverWait
 from starlette.responses import RedirectResponse
 from starlette.staticfiles import StaticFiles
 
 from logger import Logger
+
 
 app = FastAPI()
 logger = Logger().get_logger(__name__)
@@ -46,6 +51,41 @@ def search(keyword:str):
     driver.quit()
 
     return {'list': job_list}
+
+@app.post('/saramin_login') # /docs로 test 가능
+def saramin_login(id:str, pw:str):
+# 1. 드라이버 초기화
+    service = Service()
+    driver = webdriver.Chrome(service=service, options=options)
+    # 2. 페이지 열기
+    url = 'https://www.saramin.co.kr/zf_user/auth'
+    driver.get(url) # get방식으로 url을 열어줌
+    # 3. 원하는 요소 가져와서 특정 기능 수행(클릭,스크롤,입력)
+    driver.find_element(By.ID,'id').send_keys(id)
+    driver.find_element(By.ID,'password').send_keys(pw)
+    time.sleep(1) # 1초 대기
+    driver.find_element(By.CSS_SELECTOR, 'button.btn_login.BtnType.SizeML[type="submit"]').click()
+
+    # 로그인 페이지로 이동했을 때 페이지가 다 읽혀지지 않았을 경우가 있다.
+    # 그래서 여유있게 sleep을 주면 된다.
+    # time.sleep(3)
+    #특정 요소가 읽혀질때 까지 기다리게 하는 방법도 있다.
+    # WebDriverWait(driver,최대 대기시간).until(기다리는 요소)
+    gnb = (WebDriverWait(driver, 3)
+           .until(ec.presence_of_element_located((By.CSS_SELECTOR, '#sri_gnb_wrap ul.gnb'))))
+    logger.info(f'gnb:{gnb}')
+    # javascript 강제 실행
+    driver.execute_script(f'alert("안녕하세요 {id}님! 사람인 입니다.")')
+
+    time.sleep(3)
+    driver.switch_to.alert.accept() # alert을 자동으로 닫아주는 기능
+
+    list = gnb.find_elements(By.CSS_SELECTOR, 'li.only a')
+    for item in list:
+        logger.info(f'item:{item.text}')
+        if item.text == '스크랩/관심기업':
+            item.click()
+
 
 @app.get("/")
 def root():
